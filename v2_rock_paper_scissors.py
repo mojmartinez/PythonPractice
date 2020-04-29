8ee7b9a017b4d85b204ac216878e9e673be5e4eb
Manny Martinez <43285362+mojmartinez@users.noreply.github.com>
2020-04-28 20:20:30 -0500

    RPS v1 and v2 added

---
+++ v2_rock_paper_scissors.py
@@ -0,0 +1,26 @@
+print("Rock...")
+print("Paper...")
+print("Scissors...")
+
+player1 = input("Player 1, make you move: ")
+player2 = input("Player 2, make you move: ")
+
+if player1 == player2:
+    print("It's a Tie!")
+elif player1 == "rock":
+    if player2 == "scissors":
+        print("Player One Wins!")
+    elif player2 == "paper":
+        print("Player Two Wins!")
+elif player1 =="paper":
+    if player2 == "rock":
+        print("Player One Wins!")
+    elif player2 == "scissors":
+        print("Player Two Wins!")
+elif player1 == "scissors": 
+    if player2 == "rock":
+        print("Player Two Wins!")
+    elif player2 == "paper":
+        print("Player One Wins!")
+else: 
+    print("Something went Wrong")
